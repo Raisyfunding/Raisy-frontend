dbfcad47a44d07f058fd66b5afcc35cd5b1fa102
Matteo Georges <matteo.georges74@hotmail.fr>
2021-11-24 05:08:12 +0100

    feat: submit form mostly working wip

--- src/pages/Submit/Submit.jsx
+++ src/pages/Submit/Submit.jsx
@@ -1,16 +1,45 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, forwardRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
 import { useDropzone } from "react-dropzone";
 import { Screen } from "../../styles/globalStyles";
-import { Flex, Image, Input, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Flex,
+  HStack,
+  Button,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Text,
+  useColorModeValue,
+  Radio,
+  RadioGroup,
+  Stack,
+  useNumberInput,
+  Link,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+} from "@chakra-ui/react";
 import Footer from "../../components/Footer/Footer.jsx";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./components/datepicker.scss";
 
 function Submit() {
-  const inputRef = useRef(null);
   const [title, setTitle] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [staggered, setStaggered] = React.useState(false);
   const [description, setDescription] = React.useState("");
-  const [nameError, setNameError] = useState(null);
-  const handleChange = (event) => setTitle(event.target.value);
+  const [titleError, setTitleError] = useState(null);
+  const [amountError, setAmountError] = useState(null);
+  const [date, setDate] = useState(new Date());
+  const [nbMilestones, setNbMilestones] = useState(2);
+  const [pctReleasePerMilestone, setPctReleasePerMilestone] = useState([
+    50, 100,
+  ]);
+  const handleChangeTitle = (event) => setTitle(event.target.value);
+  const handleChangeAmount = (event) => setAmount(event.target.value);
 
   const thumbsContainer = {
     display: "flex",
@@ -45,9 +74,21 @@ function Submit() {
 
   const validateTitle = () => {
     if (title.length === 0) {
-      setNameError("This field can't be blank");
+      setTitleError("This field can't be blank");
     } else {
-      setNameError(null);
+      setTitleError(null);
+    }
+  };
+
+  const validAmount = (amount) => /^\d+$/.test(amount);
+
+  const validateAmount = () => {
+    if (amount.length === 0) {
+      setAmountError("This field can't be blank");
+    } else if (validAmount(amount)) {
+      setAmountError(null);
+    } else {
+      setAmountError("Invalid amount.");
     }
   };
 
@@ -73,13 +114,41 @@ function Submit() {
     </div>
   ));
 
-  useEffect(
-    () => () => {
-      // Make sure to revoke the data uris to avoid memory leaks
-      files.forEach((file) => URL.revokeObjectURL(file.preview));
+  const {
+    getInputProps: getNumberInputProps,
+    getIncrementButtonProps,
+    getDecrementButtonProps,
+  } = useNumberInput({
+    step: 1,
+    defaultValue: 2,
+    min: 2,
+    max: 5,
+    precision: 0,
+    onChange: (_, _val) => {
+      setPctReleasePerMilestone([0, ...pctReleasePerMilestone]);
+      setNbMilestones(_val);
     },
-    [files]
-  );
+  });
+
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getNumberInputProps({ isReadOnly: true });
+
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <Input
+      textAlign={"center"}
+      isRequired={"true"}
+      onClick={onClick}
+      ref={ref}
+      readOnly
+      value={value}
+    />
+  ));
+
+  useEffect(() => {
+    // Make sure to revoke the data uris to avoid memory leaks
+    files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, [files]);
 
   return (
     <Screen
@@ -121,17 +190,17 @@ function Submit() {
           colorScheme={"red"}
           placeholder="Title of the campaign"
           isRequired={"true"}
-          isInvalid={nameError}
+          isInvalid={titleError}
           onBlur={validateTitle}
           maxLength={30}
-          onChange={handleChange}
+          onChange={handleChangeTitle}
           value={title}
           borderColor={useColorModeValue("var(--black)", "var(--white)")}
           variant={title.length > 0 ? "filled" : "outline"}
           border={title.length > 0 ? "none" : "solid 1px"}
         />
         <Flex flexDirection={"row"} paddingBottom={"50px"}>
-          <Text color={"red"}>{nameError}</Text>
+          <Text color={"red"}>{titleError}</Text>
           <Text
             color={useColorModeValue("var(--black)", "var(--white)")}
             marginLeft={"auto"}
@@ -189,7 +258,7 @@ function Submit() {
           ) : (
             <p
               style={{
-                border: "dashed 2px",
+                border: "dashed 1px",
                 borderRadius: "7px",
                 paddingTop: "100px",
                 paddingBottom: "100px",
@@ -201,6 +270,144 @@ function Submit() {
           )}
         </div>
         <aside style={thumbsContainer}>{thumbs}</aside>
+        <Text
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"5px"}
+          paddingTop={"50px"}
+          fontSize={"1.5em"}
+        >
+          Amount to raise *
+        </Text>
+        <Text
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"10px"}
+          fontStyle={"italic"}
+          textAlign={"justify"}
+        >
+          Please provide the amount you want to raise.
+        </Text>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            fontSize="1.2em"
+            children="$"
+          />
+          <Input
+            colorScheme={"red"}
+            placeholder="Amount to raise"
+            isRequired={"true"}
+            isInvalid={amountError}
+            onBlur={validateAmount}
+            maxLength={10}
+            onChange={handleChangeAmount}
+            value={amount}
+            borderColor={useColorModeValue("var(--black)", "var(--white)")}
+            variant={amount.length > 0 ? "filled" : "outline"}
+            border={amount.length > 0 ? "none" : "solid 1px"}
+          />
+        </InputGroup>
+        <Flex flexDirection={"row"} paddingBottom={"50px"}>
+          <Text color={"red"}>{amountError}</Text>
+          <Text
+            color={useColorModeValue("var(--black)", "var(--white)")}
+            marginLeft={"auto"}
+          >
+            {title.length}/10
+          </Text>
+        </Flex>
+        <Text
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"5px"}
+          fontSize={"1.5em"}
+        >
+          Date of the end of the campaign *
+        </Text>
+        <Text
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"10px"}
+          fontStyle={"italic"}
+          textAlign={"justify"}
+        >
+          Please provide the date you want to end the campaign.
+        </Text>
+        <DatePicker
+          selected={date}
+          onChange={(newDate) => setDate(newDate)}
+          showTimeSelect
+          dateFormat="Pp"
+          customInput={<CustomInput />}
+        />
+        <Text
+          paddingTop={"50px"}
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"5px"}
+          fontSize={"1.5em"}
+        >
+          Staggered Release *
+        </Text>
+        <Text
+          color={useColorModeValue("var(--black)", "var(--white)")}
+          marginBottom={"10px"}
+          fontStyle={"italic"}
+          textAlign={"justify"}
+        >
+          Do you want to enable staggered release? For more info click{" "}
+          <Link color={"blue.400"}>here</Link>
+        </Text>
+        <RadioGroup onChange={() => setStaggered(!staggered)} value={staggered}>
+          <Stack direction="row">
+            <Radio
+              borderColor={useColorModeValue("var(--black)", "var(--white)")}
+              value={true}
+            >
+              Yes
+            </Radio>
+            <Radio
+              value={false}
+              borderColor={useColorModeValue("var(--black)", "var(--white)")}
+            >
+              No
+            </Radio>
+          </Stack>
+        </RadioGroup>
+        {staggered ? (
+          <>
+            <Text marginBottom={"5px"} fontSize={"1.5em"} paddingTop={"50px"}>
+              Number of milestones *
+            </Text>
+            <Text
+              marginBottom={"10px"}
+              fontStyle={"italic"}
+              textAlign={"justify"}
+            >
+              Please give the number of milestones of the staggered release.
+            </Text>
+            <HStack paddingTop={"10px"} justifyContent={"center"}>
+              <Button {...dec}>-</Button>
+              <Input
+                {...input}
+                display={"flex"}
+                textAlign={"center"}
+                width={"80px"}
+              />
+              <Button {...inc}>+</Button>
+            </HStack>
+            <RangeSlider
+              onChange={(_values) => setPctReleasePerMilestone(_values)}
+              value={pctReleasePerMilestone}
+            >
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack />
+              </RangeSliderTrack>
+              {pctReleasePerMilestone.map((_, idx) => (
+                // {console.log(idx, nbMilestones, pctReleasePerMilestone)}
+                <RangeSliderThumb index={idx} key={idx} />
+              ))}
+            </RangeSlider>
+          </>
+        ) : (
+          <></>
+        )}
       </Flex>
       <Footer />
     </Screen>
