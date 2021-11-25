bc11c2af61f34b6ce301e5db116b191a391afe1b
Evolve <Artevolve@yahoo.com>
2021-11-25 01:50:56 +0100

    fix: remove auth to fetch campaigns

--- src/api/index.js
+++ src/api/index.js
@@ -1,78 +1,75 @@
-import { ChainId } from "@sushiswap/sdk";
-import axios from "axios";
-import { EXPLORER_URL } from "../constants/network";
+import { ChainId } from '@sushiswap/sdk'
+import axios from 'axios'
+import { EXPLORER_URL } from '../constants/network'
 
 // eslint-disable-next-line no-undef
-const isMainnet = process.env.REACT_APP_ENV === "MAINNET";
+const isMainnet = process.env.REACT_APP_ENV === 'MAINNET'
 
 export const useApi = () => {
-	// eslint-disable-next-line no-unused-vars
-	const explorerUrl =
-		EXPLORER_URL[isMainnet ? ChainId.MATIC : ChainId.MATIC_TESTNET];
+  // eslint-disable-next-line no-unused-vars
+  const explorerUrl =
+    EXPLORER_URL[isMainnet ? ChainId.MATIC : ChainId.MATIC_TESTNET]
 
-	const apiUrl = isMainnet
-		? "https://api.raisyfunding.com"
-		: "https://raisy-api.herokuapp.com";
+  const apiUrl = isMainnet
+    ? 'https://api.raisyfunding.com'
+    : 'https://raisy-api.herokuapp.com'
 
-	const getNonce = async (address, authToken) => {
-		const res = await axios({
-			method: "get",
-			url: `${apiUrl}/account/nonce/${address}`,
-			headers: {
-				Authorization: `Bearer ${authToken}`,
-			},
-		});
-		return res.data;
-	};
+  const getNonce = async (address, authToken) => {
+    const res = await axios({
+      method: 'get',
+      url: `${apiUrl}/account/nonce/${address}`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    return res.data
+  }
 
-	const getAuthToken = async (address) => {
-		let result = await axios({
-			method: "post",
-			url: `${apiUrl}/auth/getToken`,
-			data: JSON.stringify({ address: address }),
-			headers: { "Content-Type": "application/json" },
-		});
-		if (result.data.status === "success") {
-			let token = result.data.token;
-			return token;
-		}
-		return null;
-	};
+  const getAuthToken = async (address) => {
+    let result = await axios({
+      method: 'post',
+      url: `${apiUrl}/auth/getToken`,
+      data: JSON.stringify({ address: address }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    if (result.data.status === 'success') {
+      let token = result.data.token
+      return token
+    }
+    return null
+  }
 
-	const getAccountDetails = async (authToken) => {
-		const res = await axios({
-			method: "get",
-			url: `${apiUrl}/account/getaccountinfo`,
-			headers: {
-				Authorization: `Bearer ${authToken}`,
-			},
-		});
+  const getAccountDetails = async (authToken) => {
+    const res = await axios({
+      method: 'get',
+      url: `${apiUrl}/account/getaccountinfo`,
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
 
-		return res.data;
-	};
+    return res.data
+  }
 
-	const fetchAllCampaigns = async (authToken) => {
-		const res = await axios({
-			method: "get",
-			url: `${apiUrl}/campaign/fetchAllCampaigns`,
-			headers: {
-				Authorization: `Bearer ${authToken}`,
-			},
-		});
-		return res.data;
-	};
+  const fetchAllCampaigns = async (authToken) => {
+    const res = await axios({
+      method: 'get',
+      url: `${apiUrl}/campaign/fetchAllCampaigns`,
+    })
+    return res.data
+  }
 
-	const fetchCampaignById = async (_id) => {
-		const res = await axios.get(`${apiUrl}/campaign/getCampaign/${_id}`);
-		return res.data;
-	};
+  const fetchCampaignById = async (_id) => {
+    const res = await axios.get(`${apiUrl}/campaign/getCampaign/${_id}`)
+    return res.data
+  }
 
-	return {
-		apiUrl,
-		getNonce,
-		getAuthToken,
-		getAccountDetails,
-		fetchAllCampaigns,
-		fetchCampaignById,
-	};
-};
+  return {
+    apiUrl,
+    getNonce,
+    getAuthToken,
+    getAccountDetails,
+    fetchAllCampaigns,
+    fetchCampaignById,
+  }
+}
