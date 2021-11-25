51fd6d7d9722fe643f9cfc45abad645403c6d4b8
Evolve <Artevolve@yahoo.com>
2021-11-25 06:26:37 +0100

    feat: fully working campaign creation
    - Add on-chain transactions when creating a new campaign
    - Minor changes
    - Upgrade to ethers@5

--- src/contracts/raisyCampaigns.js
+++ src/contracts/raisyCampaigns.js
@@ -1,76 +1,79 @@
-import { ChainId } from "@sushiswap/sdk";
+import { ChainId } from '@sushiswap/sdk';
 
-import { calculateGasMargin, getHigherGWEI } from "../utils";
-import useContract from "../hooks/useContract";
+import { calculateGasMargin, getHigherGWEI } from '../utils';
+import useContract from '../hooks/useContract';
 
-import RaisyCampaigns from "../abi/RaisyCampaigns.json";
-import { Contracts } from "../constants/network";
+import RaisyCampaigns from '../abi/RaisyCampaigns.json';
+import { Contracts } from '../constants/network';
 
 // eslint-disable-next-line no-undef
-const isMainnet = process.env.REACT_APP_ENV === "MAINNET";
-const CHAIN = isMainnet ? ChainId.FANTOM : ChainId.FANTOM_TESTNET;
+const isMainnet = process.env.REACT_APP_ENV === 'MAINNET';
+const CHAIN = isMainnet ? ChainId.MATIC : ChainId.MATIC_TESTNET;
 
 export const useCampaignsContract = () => {
-	const { getContract } = useContract();
+  const { getContract } = useContract();
 
-	const getCampaignsContract = async () => {
-		await getContract(Contracts[CHAIN].raisyCampaigns, RaisyCampaigns);
-	};
+  const getCampaignsContract = async () => {
+    return await getContract(Contracts[CHAIN].raisyCampaigns, RaisyCampaigns);
+  };
 
-	const addCampaign = async (duration, amount, from) => {
-		const contract = await getCampaignsContract();
-		const args = [duration, amount];
+  const addCampaign = async (duration, amount, from) => {
+    const contract = await getCampaignsContract();
+    const args = [duration, amount];
 
-		const options = { from, gasPrice: getHigherGWEI() };
+    const options = { from, gasPrice: getHigherGWEI() };
+    console.log(from);
 
-		const gasEstimate = await contract.estimateGas[
-			"addCampaign(uint256,uint256)"
-		](...args, options);
-		options.gasLimit = calculateGasMargin(gasEstimate);
+    const gasEstimate = await contract.estimateGas[
+      'addCampaign(uint256,uint256)'
+    ](...args, options);
+    options.gasLimit = calculateGasMargin(gasEstimate);
 
-		return await contract["addCampaign(uint256,uint256)"](...args, options);
-	};
+    console.log(gasEstimate);
 
-	const addCampaignReleaseSchedule = async (
-		duration,
-		amount,
-		nbMilestones,
-		pctReleasePerMilestone,
-		from
-	) => {
-		const contract = await getCampaignsContract();
-		const args = [duration, amount, nbMilestones, pctReleasePerMilestone];
+    return await contract['addCampaign(uint256,uint256)'](...args, options);
+  };
 
-		const options = { from, gasPrice: getHigherGWEI() };
+  const addCampaignReleaseSchedule = async (
+    duration,
+    amount,
+    nbMilestones,
+    pctReleasePerMilestone,
+    from
+  ) => {
+    const contract = await getCampaignsContract();
+    const args = [duration, amount, nbMilestones, pctReleasePerMilestone];
 
-		const gasEstimate = await contract.estimateGas[
-			"addCampaign(uint256,uint256,uint256,uint256[])"
-		](...args, options);
-		options.gasLimit = calculateGasMargin(gasEstimate);
+    const options = { from, gasPrice: getHigherGWEI() };
 
-		return await contract["addCampaign(uint256,uint256)"](...args, options);
-	};
+    const gasEstimate = await contract.estimateGas[
+      'addCampaign(uint256,uint256,uint256,uint256[])'
+    ](...args, options);
+    options.gasLimit = calculateGasMargin(gasEstimate);
 
-	const donateERC20 = async (campaignId, amount, payToken, from) => {
-		const contract = await getCampaignsContract();
+    return await contract['addCampaign(uint256,uint256,uint256,uint256[])'](...args, options);
+  };
 
-		const options = { from, gasPrice: getHigherGWEI() };
+  const donateERC20 = async (campaignId, amount, payToken, from) => {
+    const contract = await getCampaignsContract();
 
-		return await contract.donate(campaignId, amount, payToken, options);
-	};
+    const options = { from, gasPrice: getHigherGWEI() };
 
-	const claimProofOfDonation = async (campaignId, from) => {
-		const contract = await getCampaignsContract();
+    return await contract.donate(campaignId, amount, payToken, options);
+  };
 
-		const options = { from, gasPrice: getHigherGWEI() };
+  const claimProofOfDonation = async (campaignId, from) => {
+    const contract = await getCampaignsContract();
 
-		return await contract.claimProofOfDonation(campaignId, options);
-	};
+    const options = { from, gasPrice: getHigherGWEI() };
 
-	return {
-		addCampaign,
-		addCampaignReleaseSchedule,
-		donateERC20,
-		claimProofOfDonation,
-	};
+    return await contract.claimProofOfDonation(campaignId, options);
+  };
+
+  return {
+    addCampaign,
+    addCampaignReleaseSchedule,
+    donateERC20,
+    claimProofOfDonation,
+  };
 };
--- src/pages/Landingpage/components/Popular/ProjectCard.jsx
+++ src/pages/Landingpage/components/Popular/ProjectCard.jsx
@@ -1,37 +1,48 @@
-import { Flex, Image, Text, Progress } from "@chakra-ui/react";
-import React from "react";
+import { Flex, Image, Text, Progress } from '@chakra-ui/react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const ProjectCard = ({ campaign }) => {
-	return (
-		<Flex flexDirection={"column"} width={"300px"} height={"700px"}>
-			<Image
-				width={"300px"}
-				height={"300px"}
-				src={`https://cloudflare-ipfs.com/ipfs/${campaign.coverImageHash}`}
-			/>
-			<Text paddingTop={"15px"} textAlign={"justify"}>
-				{campaign.title}
-			</Text>
-			{/* <Text paddingTop={"15px"} textAlign={"justify"}>
+  const history = useHistory();
+
+  return (
+    <Flex
+      flexDirection={'column'}
+      width={'300px'}
+      height={'auto'}
+      onClick={() => {
+        history.push(`/campaign/${campaign.campaignId}`);
+      }}
+      _hover={{ cursor: 'pointer' }}
+    >
+      <Image
+        width={'300px'}
+        height={'300px'}
+        src={`https://cloudflare-ipfs.com/ipfs/${campaign.coverImageHash}`}
+      />
+      <Text paddingTop={'15px'} textAlign={'justify'}>
+        {campaign.title}
+      </Text>
+      {/* <Text paddingTop={"15px"} textAlign={"justify"}>
 				{campaign.description}
 			</Text> */}
-			{/* <Text paddingTop={"15px"} textAlign={"justify"}>
+      {/* <Text paddingTop={"15px"} textAlign={"justify"}>
         {campaign.category}
       </Text> */}
-			<Text paddingTop={"15px"} paddingBottom={"15px"} textAlign={"justify"}>
-				{campaign.nbDonations} donations
-			</Text>
-			<Progress
-				colorScheme='blue'
-				value={campaign.amountRaised / campaign.amountToRaise}
-				borderRadius={"10px"}
-				height={"4px"}
-			/>
-			<Text paddingTop={"15px"} textAlign={"justify"}>
-				{campaign.amountRaised} $ Raised out of {campaign.amountToRaise} $
-			</Text>
-		</Flex>
-	);
+      <Text paddingTop={'15px'} paddingBottom={'15px'} textAlign={'justify'}>
+        {campaign.nbDonations} donations
+      </Text>
+      <Progress
+        colorScheme="blue"
+        value={campaign.amountRaised / campaign.amountToRaise}
+        borderRadius={'10px'}
+        height={'4px'}
+      />
+      <Text paddingTop={'15px'} textAlign={'justify'}>
+        {campaign.amountRaised} $ Raised out of {campaign.amountToRaise} $
+      </Text>
+    </Flex>
+  );
 };
 
 export default ProjectCard;
