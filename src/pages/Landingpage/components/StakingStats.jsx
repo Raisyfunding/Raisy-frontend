35dfba8bfab9c1c54e0ee0b36cebdcc35f44658e
Matteo Georges <matteo.georges74@hotmail.fr>
2021-11-26 23:22:10 +0100

    feat: test new library

--- src/pages/Landingpage/components/StakingStats.jsx
+++ src/pages/Landingpage/components/StakingStats.jsx
@@ -7,16 +7,107 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import React from 'react'
-import Marquee from 'react-fast-marquee'
+import Marquee from 'react-marquee-master'
 import decentralizedwhite from '../../../images/decentralized_white.png'
 import decentralizedblack from '../../../images/decentralized_black.png'
 
 const StakingStats = () => {
   const priceRaisy = 10
+  const test = [
+    <Flex
+      _hover={{ opacity: '0.8' }}
+      width={'50%'}
+      height={'280px'}
+      backgroundColor={useColorModeValue(
+        'rgba(255,255,255,1)',
+        'rgba(21,21,21,.64)'
+      )}
+      borderRadius={'50px'}
+      border={'1px solid'}
+      borderColor={useColorModeValue(
+        'rgba(235, 235, 235, 1)',
+        'rgba(25,25,25,1)'
+      )}
+      padding={'20px'}
+      flexDirection={'column'}
+    >
+      <Flex
+        backgroundColor={useColorModeValue(
+          'rgba(250,250,250,1)',
+          'rgba(25,25,25,1)'
+        )}
+        width={'80px'}
+        height={'80px'}
+        borderRadius={'full'}
+      >
+        <Image
+          src={useColorModeValue(decentralizedblack, decentralizedwhite)}
+          width={'50px'}
+          height={'50px'}
+          margin={'auto'}
+        />
+      </Flex>
+      <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+        100 k
+      </Text>
+      <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+        campaigns <br />
+        funded
+      </Text>
+    </Flex>,
+    <Flex
+      _hover={{ opacity: '0.8' }}
+      width={'50%'}
+      height={'280px'}
+      backgroundColor={useColorModeValue(
+        'rgba(255,255,255,1)',
+        'rgba(21,21,21,.64)'
+      )}
+      borderRadius={'50px'}
+      border={'1px solid'}
+      borderColor={useColorModeValue(
+        'rgba(235, 235, 235, 1)',
+        'rgba(25,25,25,1)'
+      )}
+      padding={'20px'}
+      flexDirection={'column'}
+    >
+      <Flex
+        backgroundColor={useColorModeValue(
+          'rgba(250,250,250,1)',
+          'rgba(25,25,25,1)'
+        )}
+        width={'80px'}
+        height={'80px'}
+        borderRadius={'full'}
+      >
+        <Image
+          src={useColorModeValue(decentralizedblack, decentralizedwhite)}
+          width={'50px'}
+          height={'50px'}
+          margin={'auto'}
+        />
+      </Flex>
+      <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+        100 k
+      </Text>
+      <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+        campaigns <br />
+        funded
+      </Text>
+    </Flex>,
+  ]
   return (
     <div>
       <Flex flexDirection={'column'} width={'100vw'} height={'100vh'}>
-        <Text
+        <Marquee
+          marqueeItems={test}
+          style={{ width: '200px' }}
+          inverseMarqueeItems={true}
+          direction="down"
+          marqueeItemClassName="flex-direction: column"
+        />
+        {/* <Text
           fontSize={{ base: '4xl', md: '5xl', lg: '6xl' }}
           style={{
             textAlign: 'center',
@@ -284,7 +375,7 @@ const StakingStats = () => {
               </Marquee>
             </Flex>
           </Flex>
-        </Flex>
+        </Flex> */}
       </Flex>
     </div>
   )
