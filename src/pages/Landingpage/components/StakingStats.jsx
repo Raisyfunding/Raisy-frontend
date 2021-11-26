5e39ebb101c1b31c99ea3c9d842e6ae600f84dc4
Matteo Georges <matteo.georges74@hotmail.fr>
2021-11-26 22:25:49 +0100

    feat: not finished

--- src/pages/Landingpage/components/StakingStats.jsx
+++ src/pages/Landingpage/components/StakingStats.jsx
@@ -7,10 +7,12 @@ import {
   useColorModeValue,
 } from '@chakra-ui/react'
 import React from 'react'
+import Marquee from 'react-fast-marquee'
 import decentralizedwhite from '../../../images/decentralized_white.png'
 import decentralizedblack from '../../../images/decentralized_black.png'
 
 const StakingStats = () => {
+  const priceRaisy = 10
   return (
     <div>
       <Flex flexDirection={'column'} width={'100vw'} height={'100vh'}>
@@ -29,63 +31,115 @@ const StakingStats = () => {
           RAISY STATS
         </Text>
         <Flex
-          gridGap={'30px'}
-          flexDirection={'column'}
-          maxWidth={'50vw'}
+          flexDirection={'row'}
+          width={'100vw'}
           maxHeight={'70vw'}
-          paddingLeft={'30px'}
+          justifyContent={'center'}
         >
-          <Flex flexDirection={'row'} gridGap={'30px'}>
-            <Flex
-              width={'50%'}
-              height={'280px'}
-              backgroundColor={useColorModeValue(
-                'rgba(255,255,255,1)',
-                'rgba(21,21,21,.64)'
-              )}
-              borderRadius={'50px'}
-              border={'1px solid'}
-              borderColor={useColorModeValue(
-                'rgba(235, 235, 235, 1)',
-                'rgba(25,25,25,1)'
-              )}
-              padding={'20px'}
-              flexDirection={'column'}
-            >
+          <Flex
+            gridGap={'30px'}
+            flexDirection={'column'}
+            width={'50vw'}
+            height={'70vw'}
+            paddingLeft={'30px'}
+          >
+            <Flex flexDirection={'row'} gridGap={'30px'}>
               <Flex
+                _hover={{ opacity: '0.8' }}
+                width={'50%'}
+                height={'280px'}
                 backgroundColor={useColorModeValue(
-                  'rgba(250,250,250,1)',
+                  'rgba(255,255,255,1)',
+                  'rgba(21,21,21,.64)'
+                )}
+                borderRadius={'50px'}
+                border={'1px solid'}
+                borderColor={useColorModeValue(
+                  'rgba(235, 235, 235, 1)',
                   'rgba(25,25,25,1)'
                 )}
-                width={'80px'}
-                height={'80px'}
-                borderRadius={'full'}
+                padding={'20px'}
+                flexDirection={'column'}
               >
-                <Image
-                  src={useColorModeValue(
-                    decentralizedblack,
-                    decentralizedwhite
+                <Flex
+                  backgroundColor={useColorModeValue(
+                    'rgba(250,250,250,1)',
+                    'rgba(25,25,25,1)'
                   )}
-                  width={'50px'}
-                  height={'50px'}
-                  margin={'auto'}
-                />
+                  width={'80px'}
+                  height={'80px'}
+                  borderRadius={'full'}
+                >
+                  <Image
+                    src={useColorModeValue(
+                      decentralizedblack,
+                      decentralizedwhite
+                    )}
+                    width={'50px'}
+                    height={'50px'}
+                    margin={'auto'}
+                  />
+                </Flex>
+                <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+                  100 k
+                </Text>
+                <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+                  campaigns <br />
+                  funded
+                </Text>
               </Flex>
-              <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
-                100 k
-              </Text>
-              <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
-                campaigns <br />
-                funded
-              </Text>
+              <Flex
+                _hover={{ opacity: '0.8' }}
+                width={'50%'}
+                height={'280px'}
+                backgroundColor={useColorModeValue(
+                  'rgba(255,255,255,1)',
+                  'rgba(21,21,21,.64)'
+                )}
+                borderRadius={'50px'}
+                border={'1px solid'}
+                borderColor={useColorModeValue(
+                  'rgba(235, 235, 235, 1)',
+                  'rgba(25,25,25,1)'
+                )}
+                padding={'20px'}
+                flexDirection={'column'}
+              >
+                <Flex
+                  backgroundColor={useColorModeValue(
+                    'rgba(250,250,250,1)',
+                    'rgba(25,25,25,1)'
+                  )}
+                  width={'80px'}
+                  height={'80px'}
+                  borderRadius={'full'}
+                >
+                  <Image
+                    src={useColorModeValue(
+                      decentralizedblack,
+                      decentralizedwhite
+                    )}
+                    width={'50px'}
+                    height={'50px'}
+                    margin={'auto'}
+                  />
+                </Flex>
+                <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+                  3 billion
+                </Text>
+                <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+                  $ <br />
+                  of Donation
+                </Text>
+              </Flex>
             </Flex>
             <Flex
-              width={'50%'}
-              height={'280px'}
-              backgroundColor={useColorModeValue(
-                'rgba(255,255,255,1)',
-                'rgba(21,21,21,.64)'
-              )}
+              _hover={{ opacity: '0.8' }}
+              height={'200px'}
+              style={{
+                background:
+                  'linear-gradient(100deg, rgba(78, 213, 186, 1), rgba(191, 222, 199, 1))',
+              }}
               borderRadius={'50px'}
               border={'1px solid'}
               borderColor={useColorModeValue(
@@ -93,7 +147,7 @@ const StakingStats = () => {
                 'rgba(25,25,25,1)'
               )}
               padding={'20px'}
-              flexDirection={'column'}
+              flexDirection={'row'}
             >
               <Flex
                 backgroundColor={useColorModeValue(
@@ -115,57 +169,120 @@ const StakingStats = () => {
                 />
               </Flex>
               <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
-                3 billion
+                300k
               </Text>
-              <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
-                $ <br />
-                of Donation
+              <Text
+                fontSize={'2xl'}
+                fontWeight={'800'}
+                marginLeft={'auto'}
+                marginTop={'auto'}
+              >
+                people <br />
+                helped
               </Text>
             </Flex>
           </Flex>
-          <Flex
-            height={'200px'}
-            style={{
-              background:
-                'linear-gradient(100deg, rgba(78, 213, 186, 1), rgba(191, 222, 199, 1))',
-            }}
-            borderRadius={'50px'}
-            border={'1px solid'}
-            borderColor={useColorModeValue(
-              'rgba(235, 235, 235, 1)',
-              'rgba(25,25,25,1)'
-            )}
-            padding={'20px'}
-            flexDirection={'row'}
-          >
-            <Flex
-              backgroundColor={useColorModeValue(
-                'rgba(250,250,250,1)',
-                'rgba(25,25,25,1)'
-              )}
-              width={'80px'}
-              height={'80px'}
-              borderRadius={'full'}
-            >
-              <Image
-                src={useColorModeValue(decentralizedblack, decentralizedwhite)}
-                width={'50px'}
-                height={'50px'}
-                margin={'auto'}
-              />
+          <Flex overflow={'hidden'} maxHeight={'65vh'}>
+            <Flex transform={'rotate(90deg)'} width={'65vh'}>
+              <Marquee
+                direction={'vertical'}
+                gradientColor={useColorModeValue([255, 255, 255], [0, 0, 0])}
+                gradientWidth={'200'}
+                speed={'40'}
+              >
+                <Flex
+                  transform={'rotate(-90deg)'}
+                  _hover={{ opacity: '0.8' }}
+                  width={'150px'}
+                  height={'250px'}
+                  backgroundColor={useColorModeValue(
+                    'rgba(255,255,255,1)',
+                    'rgba(21,21,21,.64)'
+                  )}
+                  borderRadius={'50px'}
+                  border={'1px solid'}
+                  borderColor={useColorModeValue(
+                    'rgba(235, 235, 235, 1)',
+                    'rgba(25,25,25,1)'
+                  )}
+                  padding={'20px'}
+                  flexDirection={'column'}
+                >
+                  <Flex
+                    backgroundColor={useColorModeValue(
+                      'rgba(250,250,250,1)',
+                      'rgba(25,25,25,1)'
+                    )}
+                    width={'80px'}
+                    height={'80px'}
+                    borderRadius={'full'}
+                  >
+                    <Image
+                      src={useColorModeValue(
+                        decentralizedblack,
+                        decentralizedwhite
+                      )}
+                      width={'50px'}
+                      height={'50px'}
+                      margin={'auto'}
+                    />
+                  </Flex>
+                  <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+                    {priceRaisy}
+                  </Text>
+                  <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+                    Price of
+                    <br />
+                    $RSY
+                  </Text>
+                </Flex>
+                <Flex
+                  transform={'rotate(-90deg)'}
+                  _hover={{ opacity: '0.8' }}
+                  width={'80%'}
+                  height={'250px'}
+                  backgroundColor={useColorModeValue(
+                    'rgba(255,255,255,1)',
+                    'rgba(21,21,21,.64)'
+                  )}
+                  borderRadius={'50px'}
+                  border={'1px solid'}
+                  borderColor={useColorModeValue(
+                    'rgba(235, 235, 235, 1)',
+                    'rgba(25,25,25,1)'
+                  )}
+                  padding={'20px'}
+                  flexDirection={'column'}
+                >
+                  <Flex
+                    backgroundColor={useColorModeValue(
+                      'rgba(250,250,250,1)',
+                      'rgba(25,25,25,1)'
+                    )}
+                    width={'80px'}
+                    height={'80px'}
+                    borderRadius={'full'}
+                  >
+                    <Image
+                      src={useColorModeValue(
+                        decentralizedblack,
+                        decentralizedwhite
+                      )}
+                      width={'50px'}
+                      height={'50px'}
+                      margin={'auto'}
+                    />
+                  </Flex>
+                  <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
+                    3 billion
+                  </Text>
+                  <Text fontSize={'2xl'} fontWeight={'800'} marginTop={'auto'}>
+                    $ <br />
+                    of Donation
+                  </Text>
+                </Flex>
+              </Marquee>
             </Flex>
-            <Text fontSize={'5xl'} fontWeight={'900'} margin={'auto'}>
-              300k
-            </Text>
-            <Text
-              fontSize={'2xl'}
-              fontWeight={'800'}
-              marginLeft={'auto'}
-              marginTop={'auto'}
-            >
-              people <br />
-              helped
-            </Text>
           </Flex>
         </Flex>
       </Flex>
---
+++ src/pages/Landingpage/components/Test.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Image } from '@chakra-ui/react'
+
+const Test = () => {
+  return (
+    <div>
+      <div class="container blur">
+        <ul class="slider">
+          <li>
+            <Image src="../../../images/chainlink.png" />
+          </li>
+          <li>
+            <p> I was wondering if after all these years you'd like to meet</p>
+          </li>
+          <li>
+            <p>To go over everything</p>
+          </li>
+          <li>
+            <p> They say that time's supposed to heal ya</p>
+          </li>
+          <li>
+            <p> But I ain't done much healing</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Test
