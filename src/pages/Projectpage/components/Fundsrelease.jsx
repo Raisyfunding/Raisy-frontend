71b9036d985840ad62708054ca1be469a5f6b135
Evolve <Artevolve@yahoo.com>
2021-11-26 01:57:39 +0100

    fix: minor fixes

--- src/pages/Projectpage/components/Fundsrelease.jsx
+++ src/pages/Projectpage/components/Fundsrelease.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   Text,
   Box,
@@ -6,23 +6,18 @@ import {
   HStack,
   VStack,
   useColorModeValue,
-} from '@chakra-ui/react'
-import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react'
-import { useWeb3React } from '@web3-react/core'
-import Countdown from 'react-countdown'
+} from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core';
+import Countdown from 'react-countdown';
 
 const Fundsrelease = ({ currentProject, fundingover }) => {
-  const { account } = useWeb3React()
+  const { account } = useWeb3React();
 
   return (
     <div>
-      <Text textAlign={'center'} fontSize={'3xl'} fontWeight="bold">
-        Funds release plan
-      </Text>
-      {console.log(currentProject)}
       {currentProject.nbMilestones ? (
         <div>
-          {console.log('dzeffz')}
           <Table variant="simple">
             <Thead>
               <Tr>
@@ -45,13 +40,13 @@ const Fundsrelease = ({ currentProject, fundingover }) => {
       ) : (
         <div>
           <Text textAlign="center">
-            There isn't stagger release for this project, 100% of the collected
-            funds have been released !
+            There isn't staggered release for this project, 100% of the
+            collected funds have been released !
           </Text>
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Fundsrelease
+export default Fundsrelease;
--- src/pages/Projectpage/components/VoteSession.jsx
+++ src/pages/Projectpage/components/VoteSession.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Text, Box, Center, HStack, VStack } from '@chakra-ui/react'
-import { useWeb3React } from '@web3-react/core'
-import Countdown from 'react-countdown'
+import React, { useEffect } from 'react';
+import { Text, Box, Center, HStack, VStack } from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core';
+import Countdown from 'react-countdown';
+import { toLower } from 'lodash';
 
 const VoteSession = ({ currentProject, fundingover }) => {
-  const { account } = useWeb3React()
+  const { account } = useWeb3React();
 
   return (
     <div>
       {currentProject.nbMilestones ? (
         <div>
-          {console.log(account)}
-          {account === currentProject.creator ? (
+          {toLower(account) === currentProject.creator ? (
             <div></div>
           ) : (
             <div>
@@ -26,10 +26,10 @@ const VoteSession = ({ currentProject, fundingover }) => {
                   <Text fontSize="1xl" textAlign="center" width="80%">
                     {' '}
                     The campaign creator asked for a{' '}
-                    <pan style={{ fontWeight: 'bold' }}>20% </pan> fundrelease,
+                    <span style={{ fontWeight: 'bold' }}>20%</span> funds release,
                     if most of the users vote to interrupt it during the
                     meantime, the request will be canceled. 3 consecutives
-                    failed request will prompt a refund of all donors.
+                    failed request will give a refund of all donors.
                   </Text>
                 </Center>
                 <Box height="25px" />
@@ -54,14 +54,14 @@ const VoteSession = ({ currentProject, fundingover }) => {
                 <Center>
                   <Box bg="gray.800" onClick={{}} width="50%">
                     <Text margin="10px" textAlign="center">
-                      Vote here to cancel the fundrelease request{' '}
+                      Vote here to cancel the funds release request{' '}
                     </Text>
                   </Box>
                 </Center>
                 <Center>
                   <Text textAlign="center" width="50%" marginTop="10px">
                     Only donation proof owners of this campaign will be able to
-                    vote against this fundrelease request
+                    vote against this funds release request
                   </Text>
                 </Center>
                 <Box height="20px" />
@@ -78,7 +78,7 @@ const VoteSession = ({ currentProject, fundingover }) => {
         <div></div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default VoteSession
+export default VoteSession;
